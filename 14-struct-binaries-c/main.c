1aed6dd682afdec1b29df3a9b93bf8b59dc53a01
KatanaMajesty <sasha.panasenk@gmail.com>
2022-02-15 11:12:01 +0200

    14-struct-binaries-c Fixed func

--- 14-struct-binaries-c/main.c
+++ 14-struct-binaries-c/main.c
@@ -10,11 +10,11 @@ char _path[32] = "../data/14/";
 char _filename[16] = "flight_data.bin";
 
 // returns 1 if flight_dirs are equal, otherwise 0
-int fdircmp(struct flight_dir dir1, struct flight_dir dir2)
+int fdircmp(unsigned char days1[7], unsigned char days2[7])
 {
     for (int i = 0; i < 7; i++)
     {
-        if (dir1.flight_days[i] != dir2.flight_days[i])
+        if (days1[i] != days2[i])
             return 0;
     }
     // return dir1.flight_id == dir2.flight_id;
@@ -27,27 +27,35 @@ struct flight_dir get_flight_dir(enum DaysEnum days[], size_t arr_len, unsigned 
     struct flight_dir output = { // <- initialize empty-value struct
         {0, 0, 0, 0, 0, 0, 0}, 0
     };
+    unsigned char _days[7];
     for (int i = 0; i < arr_len; i++) // <- change desired days to 1
     {
         output.flight_days[days[i]] = 1;
     }
     output.flight_id = flight_id;
+    // return (struct flight_dir){.flight_days = days, .flight_id = flight_id};
     return output;
 }
 
 // returns the flight_id of a plane with minimal ticket cost at desired flight direction
 // returns NULL if no appropriate flight_id was find
-unsigned long get_min_ticket_price(struct flight_dir direction, const char* filename)
+unsigned long get_min_ticket_price(enum DaysEnum days[], size_t days_len, const char* filename)
 {
     struct flight_data* output = malloc(sizeof(struct flight_data));
     read_data(_readbuffer, filename, 0, output); // <- needed in order to assign the first ticket cost, so there are no UB (ignore unused_result)
-    float min_cost = output->plane.ticket_cost;
-    unsigned long _flight_id = output->dir.flight_id;
+    float min_cost = __FLT_MAX__; // <- bruh
+    unsigned long _flight_id;
+
+    unsigned char _u_days[7] = {0, 0, 0, 0, 0, 0, 0}; // <- temporary solution in order for program to work
+    for (int i = 0; i < days_len; i++)
+    {
+        _u_days[days[i]] = 1;
+    }
 
     // read_data will return 0 when there is no more data left in binary file
-    for (int i = 1; read_data(_readbuffer, filename, i, output); i++) // start from 1, as 0 is already in min_cost
+    for (int i = 0; read_data(_readbuffer, filename, i, output); i++) // start from 1, as 0 is already in min_cost
     {
-        if (!fdircmp(direction, output->dir)) // returns 0 if dir1 and dir2 aren't equal
+        if (!fdircmp(_u_days, output->dir.flight_days)) // returns 0 if dir1 and dir2 aren't equal
             continue;
 
         if (min_cost > output->plane.ticket_cost)
@@ -95,7 +103,7 @@ int main(void)
     remove_data(_readbuffer, filepath, 1);
     print_flight_data(_readbuffer, filepath);
 
-    printf("flight_id: %lu\n", get_min_ticket_price(get_flight_dir((enum DaysEnum[]){MONDAY, TUESDAY}, 2, 1), filepath));
+    printf("flight_id: %lu\n", get_min_ticket_price((enum DaysEnum[]){MONDAY, TUESDAY}, 2, filepath));
 
     return 0;
 }
